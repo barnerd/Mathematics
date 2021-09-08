bd3007315d76bcee33a9b8f22872176579f64c84
David Barner <dibarner@gmail.com>
2021-09-08 10:48:20 -0700

    Initial commit

---
+++ Assets/Platformer/Scripts/MapManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public Vector2 offset; // how far to offset the top left block
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
