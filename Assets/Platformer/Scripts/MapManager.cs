4b093f388f71135aca4eacbd87f8a455ec55d94d
David Barner <dibarner@gmail.com>
2021-09-08 11:51:57 -0700

    Initial Block Creation on Map
    Map Script can create a block, from a prefab, at (x,y) coordinates. Offset is set for upper left corner

--- Assets/Platformer/Scripts/MapManager.cs
+++ Assets/Platformer/Scripts/MapManager.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     public Vector2 offset; // how far to offset the top left block
+    private float gridWidth, gridHeight; // dimensions of each block
+
+    public GameObject block;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gridWidth = block.GetComponent<SpriteRenderer>().bounds.size.x;
+        gridHeight = block.GetComponent<SpriteRenderer>().bounds.size.y;
+
+
+        for (int i = 0; i < 1; i++)
+        {
+            CreateBlock(block, i, 0);
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            CreateBlock(block, i, 2);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            CreateBlock(block, i, 4);
+        }
+
+        for (int i = 0; i < 7; i++)
+        {
+            CreateBlock(block, i, 6);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void CreateBlock(GameObject _b, int _x, int _y)
+    {
+        GameObject newBlock = Instantiate(_b, new Vector3(offset.x + _x * gridWidth, offset.y - _y * gridHeight, 0), Quaternion.identity);
+        newBlock.transform.parent = this.transform;
     }
 }
